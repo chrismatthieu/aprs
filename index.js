c7495bcd996cc4e28095da70da0a7d4649772a01
Chris Matthieu <chris@matthieu.us>
2015-07-13 00:03:05 -0700

    fixed response

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
 var request = require('request');
 
 module.exports = {
-  send: function(from, to, message) {
+  send: function(from, to, message, callback) {
 
     var url = 'http://www.findu.com/cgi-bin/sendmsg.cgi';
     var propertiesObject = { fromcall:from, tocall:to, msg: message };
 
     request({url:url, qs:propertiesObject}, function(err, response, body) {
-      if(err) { console.log(err); return; }
+      if(err) { console.log(err); return callback(err); }
       // console.log('response', response);
       // console.log('body', body);
-      return 'APRS message sent.';
+      return callback(null, body);
     });
   },
 
-  get: function(from) {
+  get: function(from, callback) {
 
     var url = 'http://api.aprs.fi/api/get';
     var propertiesObject = { what:'msg', dst:from, apikey: '27286.QJTmrxJGMvlNh', format:'json' };
 
     request({url:url, qs:propertiesObject}, function(err, response, body) {
-      if(err) { console.log(err); return; }
+      if(err) { console.log(err); return callback(err); }
       // console.log('response', response);
       // console.log('body', body);
-      return body;
+      return callback(null, body);
     });
+    // request.get({url:url, qs:propertiesObject})
+    //   .end(function(err, body) {
+    //     if(err) { console.log(err); return callback(err); }
+    //     // console.log('response', response);
+    //     console.log('body', body);
+    //     callback(null, body);
+    //   });
+
+
   }
 };
--- readme.md
+++ readme.md
@@ -1,16 +1,36 @@
 Send and Receive APRS Messages
 ---
 
+```
 var aprs = require('aprs');
-var send = aprs.send(fromCallsign, toCallsign, message);
-var get = aprs.get(toCallsign);
 
+aprs.send(fromCallsign, toCallsign, message, function(err, data){
+  if (err) {
+    console.error(err);
+    return
+  }
+  console.log(data);
+});
+
+aprs.get('73S', function(err, data){
+  if (err) {
+    console.error(err);
+    return
+  }
+  console.log(data);
+});
+
+```
 
 API Notes
 ---
 
 Send APRS
+```
 http://www.findu.com/cgi-bin/sendmsg.cgi?fromcall=N7ICE&tocall=73S&msg=testing123
+```
 
 Check APRS:
+```
 http://api.aprs.fi/api/get?what=msg&dst=73S&apikey=27286.QJTmrxJGMvlNh&format=json
+```
